2987189e9ea15fa9d01fdddad48cbee02b25469f
Fernando Campos <fernandogccomap@gmail.com>
2025-11-26 19:51:06 -0300

    feat: add hot reload to dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -200,4 +200,4 @@ def update_drill(n):
     return fig
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8050, debug=False)
+    app.run_server(host='0.0.0.0', port=8050, debug=True)
